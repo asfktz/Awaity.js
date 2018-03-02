11aec6e2f3cf0c60b087315dd91a61adc0787920
asfktz <asf.ktz@gmail.com>
2018-03-02 12:59:27 +0200

    fix no concurrency case

--- src/map.js
+++ src/map.js
@@ -3,14 +3,17 @@ import resolveIterable from './__internal__/resolveIterable';
 import { toArray } from './__internal__/utils';
 
 export default function map(iterable, mapper, options = {}) {
-  if (iterable.length <= options.concurrency) {
-    return Promise.all(iterable).then(values => values.map(mapper));
+  const { concurrency } = options;
+
+  if (!concurrency || iterable.length <= concurrency) {
+    return resolveIterable(iterable).then(values => values.map(mapper));
   }
 
   const resolved = {};
+
   return resolveIterable(iterable)
     .then(concurrent({
-      limit: options.concurrency,
+      limit: concurrency,
       breakOnError: true,
       transform: mapper,
       onResolved(value, key) {
