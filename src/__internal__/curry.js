31f08103a2dd816440c809f099641d83b17978f2
asfktz <asf.ktz@gmail.com>
2018-03-03 12:33:10 +0200

    curry

--- src/__internal__/curry.js
+++ src/__internal__/curry.js
@@ -1,11 +1,11 @@
+const wrap = (fn, prevArgs) => (...newArgs) => {
+  const args = prevArgs.concat(newArgs);
+
+  return (args.length >= fn.length)
+    ? fn(...args)
+    : wrap(fn, args);
+};
+
 export default function curry(fn) {
-  const wrap = (fn, prevArgs) => (...newArgs) => {
-    const args = prevArgs.concat(newArgs);
-
-    return (args.length >= fn.length)
-      ? fn.apply(null, args)
-      : wrap(fn, args);
-  }
-
   return wrap(fn, []);
 }
