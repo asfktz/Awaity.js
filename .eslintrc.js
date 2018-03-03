99d6ba0bd553dce1d94412ef1df6c5e5e90ee29e
asfktz <asf.ktz@gmail.com>
2018-03-03 12:32:50 +0200

    quite

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,12 @@ module.exports = {
     "no-param-reassign": 0,
     "arrow-body-style": 0,
     "consistent-return": 0,
-    "import/no-extraneous-dependencies": 0
+    "import/no-extraneous-dependencies": 0,
+    "object-shorthand": 0,
+    "func-names": 0,
+    "no-empty-function": 0,
+    "prefer-arrow-callback": 0,
+    "import/prefer-default-export": 0
   },
   "env": {
     "jest/globals": true
